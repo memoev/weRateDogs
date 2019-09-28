cec135ccf86dca1ee4e958e7e9dc03c413b5c380
Guillermo Villalta <mexcelus@gmail.com>
2019-09-27 23:53:32 -0600

    generation type for values

--- weRateDogs/src/main/java/com/example/weRateDogs/Doggos.java
+++ weRateDogs/src/main/java/com/example/weRateDogs/Doggos.java
@@ -2,13 +2,14 @@ package com.example.weRateDogs;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Doggos {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String image;
