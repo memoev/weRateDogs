0cbbccc9128f50d553d27beaad7b4c3357ed866b
Guillermo Villalta <mexcelus@gmail.com>
2019-09-28 00:37:38 -0600

    imports

--- weRateDogs/src/main/java/com/example/weRateDogs/CRUD.java
+++ weRateDogs/src/main/java/com/example/weRateDogs/CRUD.java
@@ -1,7 +1,9 @@
 package com.example.weRateDogs;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CRUD extends CrudRepository<Doggos, Long>{
 
 }
