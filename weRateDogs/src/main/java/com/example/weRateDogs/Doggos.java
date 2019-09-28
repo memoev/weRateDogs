25e49b64ed19b4649dedd4b9fa3099f943b29013
Guillermo Villalta <mexcelus@gmail.com>
2019-09-27 23:35:41 -0600

    init commit

---
+++ weRateDogs/src/main/java/com/example/weRateDogs/Doggos.java
@@ -0,0 +1,27 @@
+package com.example.weRateDogs;
+
+public class Doggos {
+	
+	private String image;
+	
+	private Integer rating;
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+	
+	
+
+}
---
+++ weRateDogs/src/main/java/com/example/weRateDogs/WeRateDogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.weRateDogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeRateDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeRateDogsApplication.class, args);
+	}
+
+}
