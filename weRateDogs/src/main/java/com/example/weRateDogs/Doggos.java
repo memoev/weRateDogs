f67a9274e7e598a891b58ede994def2b88d74d76
Guillermo Villalta <mexcelus@gmail.com>
2019-09-27 23:40:24 -0600

    generate values for id

--- weRateDogs/src/main/java/com/example/weRateDogs/Doggos.java
+++ weRateDogs/src/main/java/com/example/weRateDogs/Doggos.java
@@ -1,12 +1,14 @@
 package com.example.weRateDogs;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Doggos {
 	
 	@Id
+	@GeneratedValue
 	private Long id;
 	
 	private String image;
