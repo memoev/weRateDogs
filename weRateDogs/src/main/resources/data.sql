7f6a2a2c0de95b08722fa12f513505ef49a74614
Guillermo Villalta <mexcelus@gmail.com>
2019-09-28 00:38:10 -0600

    insert default values

---
+++ weRateDogs/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO doggos(image, rating) VALUES('https://images.dog.ceo/breeds/setter-irish/n02100877_4852.jpg', 12);
+INSERT INTO doggos(image, rating) VALUES('https://images.dog.ceo/breeds/chow/n02112137_8492.jpg', 14);
+INSERT INTO doggos(image, rating) VALUES('https://images.dog.ceo/breeds/entlebucher/n02108000_1533.jpg', 13);
+
+COMMIT;
+
